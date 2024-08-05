458d692ca74995cbe06a48996d0d536289d7f1a1
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2024-08-05 23:35:11 +0530

    Time: 11 ms (44.57%), Space: 10.9 MB (19.43%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,50 +9,19 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        map<ListNode*, int> mpp; // address aur coutn ka map bna liya
-        ListNode* temp = head;
-        while (temp != NULL) {
-            mpp[temp]++; 
-            if (mpp[temp] > 1) { // agr count>1 mtlb ek address ek se zyda bar traverse hua h(cycle h)
-                return true;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+                fast=fast-> next;
+                slow=slow-> next;
             }
-            temp = temp->next;
+            if(slow==fast){
+                return true; //slow fast pointers met, means cycle exists
+            }
         }
-        // yha tk phoche ho mtlb cycle exists
-        return false;
+        return false;        
     }
 };
 
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-//         map<ListNode*,bool> mpp ;//address aur vo present hai ki nhi uska map bnaliya
-//         ListNode*temp=head;
-//         while(temp!=NULL){
-//             if(mpp[temp]==false){ //vo address pe phle traversal nhi phocha mtlb
-//                 mpp[temp]=true; //nahi hai phle se isliy etrue krdo
-//             }
-//             else{
-//                 //mtlb is address pe dubara traversal phocha hai
-//                 return true;
-//             }
-//             temp = temp-> next;
-//         }
-//         //yha tk phoche hai mtlb loop nhi h
-//         return false;
-//     }
-// };
-
-//  map<ListNode*, int> mpp; // map to store the address and its count
-//     ListNode* temp = head;
-    
-//     while (temp != NULL) {
-//         mpp[temp]++; // increment the count for the current node address
-//         if (mpp[temp] > 1) { // if the count is greater than 1, a cycle exists
-//             return true;
-//         }
-//         temp = temp->next;
-//     }
-    
-//     // If we reach here, no cycle exists
-//     return false;
