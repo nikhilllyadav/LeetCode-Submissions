347e2a719a3ea2d3c1bc7cb164991176ab3c7691
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2024-09-02 00:48:06 +0530

    Time: 11 ms (81.2%), Space: 20.7 MB (35.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,10 +3,8 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        int maxi=INT_MIN;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxi=max(maxi,nums[i]);
         }
         int totalsum= n*(n+1)/2;
         return totalsum-sum;
