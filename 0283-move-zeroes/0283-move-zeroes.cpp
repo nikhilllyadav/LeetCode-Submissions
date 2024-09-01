cdf5682fc303a4da7d3944c0956a4ec14be532a6
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2024-09-02 00:34:11 +0530

    Time: 16 ms (52.12%), Space: 22.8 MB (6.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         vector<int>temp;
-        for(int i=0;i<nums.size();i++){
+        int nonZero=0;
+        //nonzero elements ko temp me insert krdo
+        for(int i =0;i<nums.size();i++){
             if(nums[i]!=0){
                 temp.push_back(nums[i]);
+                nonZero++;
             }
         }
-        for(int i=0;i<nums.size();i++){
+        // temp ke elements ko wapis nums me insert krdo 
+        for(int i=0;i<nonZero;i++){
             nums[i]=temp[i];
         }
-        int nonZero=temp.size(); //kitne index tk non zero elements bhare h
-        for(int i =nonZero;i<nums.size();i++){
+        for(int i=nonZero;i<nums.size();i++){
             nums[i]=0;
         }
-    }    
+    }
 };
