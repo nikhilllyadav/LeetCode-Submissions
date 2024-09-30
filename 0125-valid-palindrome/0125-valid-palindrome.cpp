3b0191f8aaf47c62f01b00e5c3b6816b1b682fae
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2024-10-01 00:43:03 +0530

    Time: 6 ms (45.81%), Space: 9.7 MB (12.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(string s) {
         string result;
         for(int i =0;i<s.length();i++){
-            if(s[i]>='A' && s[i]<='Z' || s[i]>='a' && s[i]<='z'|| s[i]>=0 && s[i]<=9 ){
+            if(isalnum(s[i])){
                 result+=tolower(s[i]);
             }
         }
