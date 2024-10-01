b70f8d6af4dd86c1b36a210ae31be892e33b0753
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2024-10-01 19:27:59 +0530

    Time: 11 ms (90.73%), Space: 21.9 MB (27.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>temp;
-        int nonZero=0;
-        //nonzero elements ko temp me insert krdo
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]!=0){
-                temp.push_back(nums[i]);
-                nonZero++;
+        int n=nums.size();
+        int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i; //this makes sure j is at the first zero and than we'll point i next to it
+                break;
             }
         }
-        // temp ke elements ko wapis nums me insert krdo 
-        for(int i=0;i<nonZero;i++){
-            nums[i]=temp[i];
-        }
-        for(int i=nonZero;i<nums.size();i++){
-            nums[i]=0;
+        if (j == -1) return; //iska matlab array me koi zero hi nhi h
+        //starting the iteration from the next index to the first zero
+        for(int i= j+1;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;//j ko aage badhao coz when it's zero i to increment ho hi rha h
+            }
         }
     }
 };
