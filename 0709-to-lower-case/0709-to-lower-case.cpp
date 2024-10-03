1c3ccef3bde0703afd05d524becdcc55ec3d0488
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2024-10-03 11:19:47 +0530

    Time: 3 ms (40.96%), Space: 7.7 MB (11.76%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.length();
         for(int i=0;i<n;i++){
             if(s[i]>='A' && s[i]<='Z'){
-                s[i]=s[i] - 'A' + 'a';
+                s[i]=tolower(s[i]);
             }
         }
         return s;
