2cd055f42c4e085097fa2fa0d37991b108b5e480
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2024-11-26 22:57:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0; // To count the number of breaks in the sorted order
-        for (int i = 1; i < n; i++) {
-            // Check if the current element is greater than the next element
-            if (nums[i-1] > nums[i]) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i-1]>nums[i]){
                 count++;
             }
         }
-        //comparison between the last and the first element
-        if(nums[n-1]>nums[0]){
-            count++;
-        } 
-        return count<=1;   
+        if(count>1){
+            return false;
+        }
+        return true;
     }
 };
+
