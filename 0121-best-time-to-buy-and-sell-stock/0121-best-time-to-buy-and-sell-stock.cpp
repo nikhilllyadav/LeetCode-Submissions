731a67f46a33c14c7e79796a383a3f47c5cda04d
Nikhil Kumar Yadav <114252081+nikhilllyadav@users.noreply.github.com>
2025-02-05 23:13:13 +0530

    Time: 0 ms (100%), Space: 97.4 MB (60.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,13 +3,13 @@ public:
     int maxProfit(vector<int>& prices) {
         int buy=prices[0];
         int profit=0;
-        for(int i=0;i<prices.size();i++){
+        for(int i =0;i<prices.size();i++){
             if(prices[i]<buy){
-                buy=prices[i]; //buy price should be minimum
+                buy = prices[i];
             }
-            else{ //matlab prices buy se bada hai
-                int sell = prices[i] - buy ; //calculating potential profit at each index
-                profit = max(sell,profit); 
+            else{
+                int sell = prices[i] - buy;
+                profit = max(sell, profit); 
             }
         }
         return profit;
